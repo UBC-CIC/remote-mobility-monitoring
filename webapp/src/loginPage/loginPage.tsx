82b90aa86565946d7984644f73cc5a4b2c2c772b
aniketc12 <aniketc12@outlook.com>
2023-01-22 19:04:25 -0800

    added some code for login

---
+++ webapp/src/loginPage/loginPage.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+function loginPage() {
+    return (
+        <div>
+            <div className='welcome'>
+                <h1>Sign in to</h1>
+                <h2>Mobility Monitor</h2>
+                <p>Organization administrators can <br /> Login here</p>
+            </div>
+        </div>
+    )
+}
+
