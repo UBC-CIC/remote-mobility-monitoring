d6047218e7d275b8b4548ef625fef74fa48c4341
aniketc12 <aniketc12@outlook.com>
2023-01-22 22:33:16 -0800

    loginpage components made

--- webapp/src/loginPage/loginPage.tsx
+++ webapp/src/loginPage/loginPage.tsx
@@ -1,14 +1,23 @@
 import React, {useState} from 'react';
+import './loginPage.css'
 
-function loginPage() {
+function LoginPage() {
     return (
-        <div>
-            <div className='welcome'>
+        <div className='wrapper'>
+            <div className='login'>
                 <h1>Sign in to</h1>
                 <h2>Mobility Monitor</h2>
                 <p>Organization administrators can <br /> Login here</p>
             </div>
+            <div className='login'>
+                <h2>Sign in</h2>
+                <input placeholder='Username'></input>
+                <br />
+                <input type='password' placeholder='Password'></input>
+            </div>
         </div>
-    )
+    );
 }
+
+export default LoginPage;
 
