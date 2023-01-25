f8cf1eb7427f5b0107c30dac80764ec5c74c8aed
aniketc12 <aniketc12@outlook.com>
2023-01-24 18:59:21 -0800

    Revert "Completed login component"
    This reverts commit 433f2134160a6c7eb161c39d1c692bbc7e8a1f7e.

--- webapp/src/loginPage/loginPage.tsx
+++ webapp/src/loginPage/loginPage.tsx
@@ -2,33 +2,18 @@ import React, {useState} from 'react';
 import './loginPage.css'
 
 function LoginPage() {
-    const [loginType, setLoginType] = useState('caregiver');
-    const handleLogin = (event) => {
-        event.preventDefault();
-        console.log(loginType);
-    }
-    const toggleLoginType = () => {
-        if (loginType === 'caregiver') setLoginType('admin')
-        else setLoginType('caregiver')
-    }
-
     return (
         <div className='wrapper'>
             <div className='login'>
                 <h1>Sign in to</h1>
                 <h2>Mobility Monitor</h2>
-                <p>{loginType === 'caregiver' ? "Organization administrators can": "Caregivers can"}<br /> 
-                <span className='alternate' onClick={toggleLoginType}>Login here</span></p>
+                <p>Organization administrators can <br /> Login here</p>
             </div>
-            <div className='login user'>
-            <h2>Sign in</h2>
-                <div className='login-input'>
-                    <input placeholder='Username'></input>
-                    <br />
-                    <input type='password' placeholder='Password'></input>
-                    <div className='forgot'>Forgot password?</div>
-                    <button type='submit' onClick={handleLogin}>Login</button>
-                </div>
+            <div className='login'>
+                <h2>Sign in</h2>
+                <input placeholder='Username'></input>
+                <br />
+                <input type='password' placeholder='Password'></input>
             </div>
         </div>
     );
