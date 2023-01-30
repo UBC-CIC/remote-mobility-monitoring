6d61845dbcdb7c2446435bfcdd7785fdc5c445d2
Yoruio <zeleidu@gmail.com>
2023-01-30 11:21:11 -0800

    Adds cognito and timestream stack

--- backend/lib/index.ts
+++ backend/lib/index.ts
@@ -1,8 +1,12 @@
 import { App } from 'aws-cdk-lib';
 import { DynamoDbStack } from './stack/dynamodb-stack';
 import { LambdaStack } from './stack/lambda-stack';
+import { CognitoStack } from './stack/cognito-stack';
+import { TimestreamStack } from './stack/timestream-stack';
 
 const app = new App();
 new DynamoDbStack(app, 'RemoteMobilityMonitoringDynamoStack');
-new LambdaStack(app, 'RemoteMobilityMonitoringLambdaStack')
+new LambdaStack(app, 'RemoteMobilityMonitoringLambdaStack');
+new CognitoStack(app, 'RemoteMobilityMonitoringCognitoStack');
+new TimestreamStack(app, 'RemoteMobilityMonitoringTimeStreamStack');
 app.synth();
