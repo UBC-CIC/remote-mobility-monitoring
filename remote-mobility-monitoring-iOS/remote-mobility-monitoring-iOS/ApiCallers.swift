c08d0f369ef5758d51e9d46aaf0ed58d82aea31b
Hung Nguyen <hachi.codingcat@gmail.com>
2023-02-09 13:45:14 -0800

    making request to API

---
+++ remote-mobility-monitoring-iOS/remote-mobility-monitoring-iOS/ApiCallers.swift
@@ -0,0 +1,56 @@
+//
+//  ApiCallers.swift
+//  remote-mobility-monitoring-iOS
+//
+//  Created by Hachi on 2023-02-09.
+//
+
+import Foundation
+
+enum ApiError: Error {
+    case invalidEndpoint
+    case invalidResponse
+    case failedToVerifyPatient
+}
+
+func verifyPatient(patientId: String, caregiverId: String, authCode: String, deviceId: String, completion: @escaping (Result<[String: Any], Error>) -> Void) {
+    let endpoint = "https://api_endpoint/patients/\(patientId)/verify"
+    let body: [String: Any] = [
+        "caregiver_id": caregiverId,
+        "auth_code": authCode,
+        "device_id": deviceId
+    ]
+    guard let url = URL(string: endpoint) else {
+        completion(.failure(ApiError.invalidEndpoint))
+        return
+    }
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.httpBody = try? JSONSerialization.data(withJSONObject: body)
+
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        if let error = error {
+            completion(.failure(error))
+            return
+        }
+
+        guard let data = data, let response = response as? HTTPURLResponse else {
+            completion(.failure(ApiError.invalidResponse))
+            return
+        }
+        if response.statusCode != 200 {
+            completion(.failure(ApiError.failedToVerifyPatient))
+            return
+        }
+
+        do {
+            let responseObject = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            completion(.success(responseObject!))
+        } catch {
+            completion(.failure(error))
+        }
+    }
+
+    task.resume()
+}
--- remote-mobility-monitoring-iOS/remote-mobility-monitoring-iOS/RegisterView.swift
+++ remote-mobility-monitoring-iOS/remote-mobility-monitoring-iOS/RegisterView.swift
@@ -7,22 +7,93 @@
 import SwiftUI
 import HealthKit
 import CodeScanner
+import KeychainAccess
 
 struct RegisterView: View {
     @State private var isScanning = false
+    @State private var patientId: String = ""
+    @State private var authCode: String = ""
+    @State private var caregiverId: String = ""
+    @State private var deviceIdDebug: String = ""
+    @State private var errorMessage: String = ""
+    @State private var errorScanning: Bool = false
+    let keychain = Keychain(service: "com.example.remote-mobility-monitoring-iOS")
     
-    func handleScanResult(result: Result<ScanResult, ScanError>) {
-        isScanning = false;
-        
-        switch result {
-        case .success(let result):
-            print(result)
+    func getDeviceId() -> String {
+        if let storedDeviceId = self.keychain["deviceId"] {
+            return storedDeviceId
+        } else {
+            guard let newDeviceId = UIDevice.current.identifierForVendor?.uuidString else {
+                errorScanning = true
+                errorMessage = "Failed to retrieve deviceId from UIDevice, please contact developers"
+                return ""
+            }
             
-        case .failure(let error):
-            print("Scanning failed: \(error.localizedDescription)")
+            do {
+                try keychain.set("deviceId", key: newDeviceId)
+            } catch {
+                errorScanning = true
+                errorMessage = "Failed to persist deviceId, please contact developers"
+            }
+            
+            return newDeviceId
         }
     }
     
+    func handleScanResult(result: Result<ScanResult, ScanError>) {
+        isScanning = false
+        
+        switch result {
+        case .success(let result):
+            let jsonString = result.string
+            
+            let jsonData = jsonString.data(using: .utf8)!
+
+            do {
+                let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: String]
+                patientId = jsonObject?["patient_id"] ?? ""
+                authCode = jsonObject?["auth_code"] ?? ""
+                caregiverId = jsonObject?["caregiver_id"] ?? ""
+                let deviceId = getDeviceId()
+                
+                if (errorScanning == false && patientId != "" && authCode != "" && caregiverId != "" && deviceId != "") {
+                    verifyPatient(patientId: patientId, caregiverId: caregiverId, authCode: authCode, deviceId: deviceId) { result in
+                        switch result {
+                            case .success(let response):
+                                print(response)
+                            case .failure(let error):
+                                errorScanning = true
+                                errorMessage = "Failed to verify patient: \(error.localizedDescription)"
+                                print("Verification failed: \(error.localizedDescription)")
+                        }
+                    }
+                } else {
+                    errorScanning = true
+                    if patientId == "" {
+                        errorMessage = "Failed to get patientId from QR code. Failed to verify."
+                    } else if authCode == "" {
+                        errorMessage = "Failed to get authCode from QR code. Failed to verify."
+                    } else if caregiverId == "" {
+                        errorMessage = "Failed to get caregiverId from QR code. Failed to verify."
+                    } else if deviceId == "" {
+                        errorMessage = "Failed to get deviceId from. Failed to verify."
+                    } else {
+                        errorMessage = "Unknown error occurred. Failed to verify."
+                    }
+                }
+            } catch {
+                errorScanning = true
+                errorMessage = "Failed to read QR code: \(error.localizedDescription)"
+                print("Error while parsing JSON: \(error.localizedDescription)")
+            }
+            
+        case .failure(let error):
+            errorScanning = true
+            errorMessage = "Scanning failed: \(error.localizedDescription)"
+            print("Scanning failed: \(error.localizedDescription)")
+        }
+    }
+
     var body: some View {
         NavigationView {
             VStack() {
@@ -51,6 +122,22 @@ struct RegisterView: View {
                     }
                 }
                 Spacer()
+                VStack(alignment: .leading) {
+                    Text("Patient ID: \(patientId)")
+                        .font(.headline)
+                    Text("Auth Code: \(authCode)")
+                        .font(.headline)
+                    Text("Caregiver ID: \(caregiverId)")
+                        .font(.headline)
+                    Text("Device Id: \(deviceIdDebug)")
+                        .font(.headline)
+                    if errorScanning {
+                        Text(errorMessage)
+                            .font(.body)
+                            .foregroundColor(.red)
+                    }
+                }
+                .padding()
                 NavigationLink(destination: MobilityView()) {
                     Text("Submit")
                         .font(ButtonStyling.font)
@@ -63,16 +150,8 @@ struct RegisterView: View {
             }
             .padding(.horizontal, 32)
             .sheet(isPresented: $isScanning) {
-                CodeScannerView(codeTypes: [.qr], simulatedData: "Hachi dancing", completion: handleScanResult(result:))
+                CodeScannerView(codeTypes: [.qr], completion: handleScanResult(result:))
             }
         }
-    }
-}
-
-
-
-struct RegisterView_Previews: PreviewProvider {
-    static var previews: some View {
-        RegisterView()
     }
 }
