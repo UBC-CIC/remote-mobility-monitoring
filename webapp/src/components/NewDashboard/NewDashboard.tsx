5afbc055f8dac33196cd1a0a6225921d329ffa0b
Leo <yenong_sun@163.com>
2023-03-14 17:00:14 -0700

    create graphs and tables with dummy data

--- webapp/src/components/NewDashboard/NewDashboard.tsx
+++ webapp/src/components/NewDashboard/NewDashboard.tsx
@@ -1,115 +1,102 @@
-import React, { useState } from "react";
-//import Sidebar, { Patient } from "./Sidebar";
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import sampleData from "./sampleData";
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-/*
- if all patients or no patient is selected,  the dashboard should display one graph per mobility metric per patient. In the end there is a summary table that contains all that patient's mobility data.
-I want to display the data in the following format:
-1 one chart/graph for each mobility metric that describes the change of metric with respect to time.
-2 each patient name will be followed by 5 charts/graphs plus a table that summarize all his data
-3 distinguish different patients sections
- */
+interface Metric {
+  patient_id: string;
+  metric_name: string;
+  metric_value: string;
+  timestamp: string;
+}
+
 interface Patient {
-    name: string;
-    gender: string;
-    age: number;
-    mobilityData: {
-      date: string;
-      stepLength: number;
-      doubleSupportTime: number;
-      walkingSpeed: number;
-      walkingAsymmetry: number;
-      distanceWalked: number;
-    }[];
-  }
-  
-interface SidebarProps {
-    patients: Patient[];
-  }
+  name: string;
+  metrics: Metric[];
+}
 
-interface PatientListProps {
-    patients: Patient[];
-    filteredMobilityData: {
-      date: string;
-      stepLength: number;
-      doubleSupportTime: number;
-      walkingSpeed: number;
-      walkingAsymmetry: number;
-      distanceWalked: number;
-    }[];
-  }
+interface Props {
+  patients: Patient[];
+}
 
-function NewDashboard({ patients }: SidebarProps) {
-    const [selectedPatient, setSelectedPatient] = useState('All Patients');
-    const [startDate, setStartDate] = useState<Date | null>(null);
-    const [endDate, setEndDate] = useState<Date | null>(null);
+function NewDashboard({ patients }: Props) {
+  const renderGraphs = (patient: Patient) => {
+    const stepLengthData = patient.metrics.filter(metric => metric.metric_name === 'step_length');
+    const doubleSupportTimeData = patient.metrics.filter(metric => metric.metric_name === 'double_support_time');
+    const walkingSpeedData = patient.metrics.filter(metric => metric.metric_name === 'walking_speed');
+    const walkingAsymmetryData = patient.metrics.filter(metric => metric.metric_name === 'walking_asymmetry');
+    const distanceWalkedData = patient.metrics.filter(metric => metric.metric_name === 'distance_walked');
   
-    const filteredPatients = selectedPatient === 'All Patients' ? patients : [patients.find(p => p.name === selectedPatient)].filter(Boolean);
-  
-    const filteredMobilityData = filteredPatients.flatMap(p => (p?.mobilityData ?? []).filter(d => {
-        const date = new Date(d.date);
-        const previousDate = startDate ? new Date(startDate) : new Date(0);
-        previousDate.setDate(previousDate.getDate() - 1);
-        return (!startDate || date >= previousDate) && (!endDate || date <= endDate);
-      }));
-    
-      return (
-        <div className="dashboard">
-          <div className="sidebar">
-            <h3>Patients</h3>
-            <select value={selectedPatient} onChange={e => setSelectedPatient(e.target.value)}>
-              <option value="All Patients">All Patients</option>
-              {patients.map(patient => (
-                <option key={patient.name} value={patient.name}>
-                  {patient.name}
-                </option>
-              ))}
-            </select>
-            <div>
-              <label htmlFor="startDatePicker">Start</label>
-              <DatePicker
-                id="startDatePicker"
-                selected={startDate}
-                onChange={date => setStartDate(date as Date)}
-                dateFormat="yyyy-MM-dd"
-                maxDate={endDate}
-              />
-            </div>
-            <div>
-              <label htmlFor="endDatePicker">End</label>
-              <DatePicker
-                id="endDatePicker"
-                selected={endDate}
-                onChange={date => setEndDate(date as Date)}
-                dateFormat="yyyy-MM-dd"
-                minDate={startDate}
-              />
-            </div>
-          </div>
-            <div className="patient-list">
-              <h3>Patient List</h3>
-              <ul>
-                {filteredMobilityData.map(data => {
-                  const patient = patients.find(p => p.mobilityData.includes(data));
-                  return (
-                    <li key={data.date}>
-                      <h4>{patient?.name ?? 'Unknown Patient'}</h4>
-                      <p>Date: {data.date}</p>
-                      <p>Step Length: {data.stepLength}</p>
-                      <p>Double Support Time: {data.doubleSupportTime}</p>
-                      <p>Walking Speed: {data.walkingSpeed}</p>
-                      <p>Walking Asymmetry: {data.walkingAsymmetry}</p>
-                      <p>Distance Walked: {data.distanceWalked}</p>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
+    return (
+      <>
+        <div>
+          <h3>Step Length</h3>
+          <LineChart
+            data={stepLengthData.map(metric => ({ x: new Date(metric.timestamp), y: parseFloat(metric.metric_value) }))}
+          />
         </div>
-      );
-      
+        <div>
+          <h3>Double Support Time</h3>
+          <LineChart
+            data={doubleSupportTimeData.map(metric => ({ x: new Date(metric.timestamp), y: parseFloat(metric.metric_value) }))}
+          />
+        </div>
+        <div>
+          <h3>Walking Speed</h3>
+          <LineChart
+            data={walkingSpeedData.map(metric => ({ x: new Date(metric.timestamp), y: parseFloat(metric.metric_value) }))}
+          />
+        </div>
+        <div>
+          <h3>Walking Asymmetry</h3>
+          <LineChart
+            data={walkingAsymmetryData.map(metric => ({ x: new Date(metric.timestamp), y: parseFloat(metric.metric_value) }))}
+          />
+        </div>
+        <div>
+          <h3>Distance Walked</h3>
+          <LineChart
+            data={distanceWalkedData.map(metric => ({ x: new Date(metric.timestamp), y: parseFloat(metric.metric_value) }))}
+          />
+        </div>
+      </>
+    );
+  };
+  
+
+  const renderTable = (patient: Patient) => {
+    // Use the patient's metrics data to render a summary table
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Metric Name</th>
+            <th>Metric Value</th>
+            <th>Timestamp</th>
+          </tr>
+        </thead>
+        <tbody>
+          {patient.metrics.map(metric => (
+            <tr key={metric.timestamp}>
+              <td>{metric.metric_name}</td>
+              <td>{metric.metric_value}</td>
+              <td>{metric.timestamp}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+
+  return (
+    <div>
+      {patients.map(patient => (
+        <div key={patient.name}>
+          <h2>{patient.name}</h2>
+          {renderGraphs(patient)}
+          {renderTable(patient)}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default NewDashboard;
