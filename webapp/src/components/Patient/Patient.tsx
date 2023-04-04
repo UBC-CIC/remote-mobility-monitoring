01f3286e3a4f313d3c7cdccf05129e6d5335ef48
aniketc12 <aniketc12@outlook.com>
2023-04-04 01:33:45 -0700

    Working on patient dashboard

--- webapp/src/components/Patient/Patient.tsx
+++ webapp/src/components/Patient/Patient.tsx
@@ -2,7 +2,14 @@ import React, {useState, useEffect} from "react";
 import { useNavigate, useParams} from "react-router-dom";
 import {ServiceHandler} from "../../helpers/ServiceHandler";
 import CaregiverNavbar from "../Navbar/CaregiverNavbar";
+import Divider from "@mui/material/Divider";
 import {decrypt} from "../../helpers/Crypto";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import {ToggleButton, ToggleButtonGroup} from "@mui/material";
+import "./Patient.css";
 
 
 function Patient() {
@@ -10,6 +17,8 @@ function Patient() {
     const [patientDetails, setPatientDetails]: any = useState({});
     const {patientIdEncrypt} = useParams();
     const [deletionMessage, setDeletionMessage] = useState("");
+    const [intervals, setIntervals] = useState("1D");
+    const [selectedPatient, setSelectedPatient] = useState("");
     const patientId = decrypt(patientIdEncrypt);
 
 
@@ -18,6 +27,7 @@ function Patient() {
         ServiceHandler.getPatient(patientId)
             .then((data: any) => {
                 setPatientDetails(data);
+                console.log(data);
             })
             .catch((err) => console.log(err));
         ServiceHandler.queryMetrics([patientId], {"year": 2022, "month": 3, "day":20}, 
@@ -27,6 +37,14 @@ function Patient() {
             })
             .catch((err) => console.log(err));
     }, []);
+
+    const handleIntervals = (e: any) => {
+        setIntervals(e.target.value);
+    };
+
+    const handlePatientChange = (e: any) => {
+        setSelectedPatient(e.target.value);
+    };
 
     const handleDeletePatient = () => {
         ServiceHandler.deletePatient(patientId!)
@@ -39,20 +57,38 @@ function Patient() {
     return (
         <>
             <CaregiverNavbar/>
-            <div><button onClick={(e) => nav("/dashboard")}>
-            Dashboard
-            </button>
-            <br/>
-            <br/>
-            <div>{JSON.stringify(patientDetails)}</div>
+            <div className="patient-container">
+                <div className="filters">
+                    <div className="padding"></div>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Patient</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={selectedPatient}
+                            label="Patient"
+                            onChange={handlePatientChange}
+                        >
+                        </Select>
+                    </FormControl>
+                    <div className="padding"></div>
+                    <ToggleButtonGroup
+                        color="primary"
+                        value={intervals}
+                        exclusive
+                        onChange={handleIntervals}
+                        aria-label="Platform"
+                    >
+                        <ToggleButton value="1D">1D</ToggleButton>
+                        <ToggleButton value="1W">1W</ToggleButton>
+                        <ToggleButton value="1M">1M</ToggleButton>
+                        <ToggleButton value="1Y">1Y</ToggleButton>
+                    </ToggleButtonGroup>
+
+                </div>
+                <div className="data">b</div>
+
             </div>
-            <button onClick={handleDeletePatient}>Delete Patient</button>
-            <br />
-            <br />
-            {deletionMessage && (
-                <div>{deletionMessage}</div>
-            )}
-            {patientDetails.is_primary? "Hello": "Not primary"}<button onClick={(e) => nav("share")}>Share</button>
         </>
     );
 }
